e33ad067098413b60d61af02bc1378325d175114
James Cook <james.cook@utoronto.ca>
2013-03-03 00:18:57 -0800

    Update GnuTLS from 3.1.3 to 3.1.9.
    This addresses a security advisory.

--- pkgs/development/libraries/gnutls/default.nix
+++ pkgs/development/libraries/gnutls/default.nix
@@ -5,11 +5,11 @@ assert guileBindings -> guile != null;
 
 stdenv.mkDerivation (rec {
 
-  name = "gnutls-3.1.3";
+  name = "gnutls-3.1.9";
 
   src = fetchurl {
-    url = "mirror://gnu/gnutls/${name}.tar.xz";
-    sha256 = "0fff9frz0ycbnppfn0w4a2s9x27k21l4hh9zbax3v7a8cg33dcpw";
+    url = "mirror://gnupg/gnutls/v3.1/${name}.tar.xz";
+    sha256 = "0gkwhz7sypfy39jfj2yzrngbxq5j9l9smqc89mqlqsh25spc8009";
   };
 
   # Note: GMP is a dependency of Nettle, whose public headers include
