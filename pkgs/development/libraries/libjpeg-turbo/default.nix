9fe1dd70261b1aa9111e12597cb9e8960b9c5642
James Cook <james.cook@utoronto.ca>
2013-12-20 20:39:13 -0800

    Unmarked libjpeg-turbo as vulnerable, now that I've merged the security patch.

--- pkgs/development/libraries/libjpeg-turbo/default.nix
+++ pkgs/development/libraries/libjpeg-turbo/default.nix
@@ -4,9 +4,8 @@ stdenv.mkDerivation rec {
   name = "libjpeg-turbo-1.3.0";
 
   src = fetchurl {
-    url = "mirror://sourceforge/libjpeg-turbo/${name}.tar.gz_vulnerable_through_1.3.0";
-    #sha256 = "0d0jwdmj3h89bxdxlwrys2mw18mqcj4rzgb5l2ndpah8zj600mr6";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "mirror://sourceforge/libjpeg-turbo/${name}.tar.gz";
+    sha256 = "0d0jwdmj3h89bxdxlwrys2mw18mqcj4rzgb5l2ndpah8zj600mr6";
   };
 
   patches = [ ./libjpeg-turbo-1.3.0-CVE-2013-6629-and-6630.patch ];
