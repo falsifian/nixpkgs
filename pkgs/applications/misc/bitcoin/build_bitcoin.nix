64c57694715f5ef61d96a9662224d918b6145f48
James Cook <james.cook@utoronto.ca>
2014-02-09 15:36:44 -0800

    bitcoin and altcoins: name of installed binary
    Install to "bitcoind" rather than "bitcoin", for consistency with
    upstream and other distros.

--- pkgs/applications/misc/bitcoin/build_bitcoin.nix
+++ pkgs/applications/misc/bitcoin/build_bitcoin.nix
@@ -14,7 +14,7 @@ let
     buildInputs = [ openssl db4 boost zlib miniupnpc ] ++ (if headless then [] else [ qt4 ]) ++ a.extraBuildInputs or [];
     configurePhase = if headless then "" else "qmake";
     installPhase = if headless then
-      ''install -D "${a.nameBase}d" "$out/bin/${a.nameBase}"''
+      ''install -D "${a.nameBase}d" "$out/bin/${a.nameBase}d"''
       else
       ''install -D "${a.nameBase}-qt" "$out/bin/${a.nameBase}-qt"'';
     preBuild = if headless then "cd src" else "";
