0d254b14847aaa0cea05d9aeb368184c1c25f20a
James Cook <james.cook@utoronto.ca>
2014-04-28 14:12:23 -0700

    Marked libmms as vulnerable.

--- pkgs/development/libraries/libmms/default.nix
+++ pkgs/development/libraries/libmms/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "libmms-0.6.2";
 
   src = fetchurl {
-    url = "mirror://sourceforge/libmms/${name}.tar.gz";
+    url = "mirror://sourceforge/libmms/${name}.tar.gz_VULNERABLE_CVE-2014-2892";
     sha256 = "0cm4gq5jm8wj04biai6cyvlvdwv286vb3ycyzi870z9d2xi1p4q1";
   };
 
