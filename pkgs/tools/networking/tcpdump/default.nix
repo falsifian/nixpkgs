cec73615d767affaccdc320aac4bdbfa47c30eb7
James Cook <james.cook@utoronto.ca>
2014-12-04 13:44:13 -0800

    Marked tcpdump vulnerable.

--- pkgs/tools/networking/tcpdump/default.nix
+++ pkgs/tools/networking/tcpdump/default.nix
@@ -4,8 +4,10 @@ stdenv.mkDerivation rec {
   name = "tcpdump-4.6.2";
 
   src = fetchurl {
-    url = "http://www.tcpdump.org/release/${name}.tar.gz";
-    sha256 = "1f701387jyxq7rjhv4hiig3b3g55m4b4403rd0zncv1sx3cf8kjj";
+    # Vulnerable to CVE-2014-8767, maybe CVE-2014-8768..9, CVE-2014-9140.
+    url = "VULNERABLE_http://www.tcpdump.org/release/${name}.tar.gz";
+    # sha256 = "1f701387jyxq7rjhv4hiig3b3g55m4b4403rd0zncv1sx3cf8kjj";
+    sha256 = "0000000000000000000000000000000000000000000000000000";
   };
 
   buildInputs = [ libpcap ];
