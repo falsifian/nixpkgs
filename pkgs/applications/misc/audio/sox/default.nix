c03016b9d2c6c61772b0f1ed46d793ba58313434
James Cook <james.cook@utoronto.ca>
2015-02-25 20:01:14 -0800

    Unmark sox vulnerable.

--- pkgs/applications/misc/audio/sox/default.nix
+++ pkgs/applications/misc/audio/sox/default.nix
@@ -14,10 +14,8 @@ stdenv.mkDerivation rec {
   name = "sox-14.4.1";
 
   src = fetchurl {
-    # Vulnerable to CVE-CVE-2014-8145.
-    url = "VULNERABLE_mirror://sourceforge/sox/${name}.tar.gz";
-    # sha256 = "16x8gykfjdhxg0kdxwzcwgwpm5caa08y2mx18siqsq0ywmpjr34s";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    url = "mirror://sourceforge/sox/${name}.tar.gz";
+    sha256 = "16x8gykfjdhxg0kdxwzcwgwpm5caa08y2mx18siqsq0ywmpjr34s";
   };
 
   patches = [
