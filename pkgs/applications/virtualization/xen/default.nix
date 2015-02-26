f9eccdde439078de79366ff4c2e2c29c4a7a64fe
James Cook <james.cook@utoronto.ca>
2015-02-25 20:51:00 -0800

    Brought previous commit more in line with master.

--- pkgs/applications/virtualization/xen/default.nix
+++ pkgs/applications/virtualization/xen/default.nix
@@ -68,9 +68,9 @@ let
       { url = http://xenbits.xensource.com/xen-extfiles/ipxe-git-9a93db3f0947484e30e753bbd61a10b17336e20e.tar.gz;
         sha256 = "0p206zaxlhda60ci33h9gipi5gm46fvvsm6k5c0w7b6cjg0yhb33";
       }
-      { url = "http://xenbits.xensource.com/xen-extfiles/polarssl-1.1.4-gpl.tgz";
+      { url = http://xenbits.xensource.com/xen-extfiles/polarssl-1.1.4-gpl.tgz;
         sha256 = "1dl4fprpwagv9akwqpb62qwqvh24i50znadxwvd2kfnhl02gsa9d";
-     }
+      }
       { url = http://xenbits.xensource.com/xen-extfiles/gmp-4.3.2.tar.bz2;
         sha256 = "0x8prpqi9amfcmi7r4zrza609ai9529pjaq0h4aw51i867064qck";
       }
